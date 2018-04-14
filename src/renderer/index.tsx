e2b45eaa299017c5389a9a7cc07188f21cfcb416
45deg <45deg@users.noreply.github.com>
2018-04-15 00:17:49 +0900

    Added alwaysOnTop option

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,6 @@
 // Initial welcome page. Delete the following line to remove it.
 import { app } from "hyperapp"
-import { ipcRenderer } from "electron"
+import { remote, ipcRenderer } from "electron"
 
 import { state } from "./states/State"
 import { actions } from "./actions/Action"
@@ -14,10 +14,14 @@ import 'xel/images/icons.svg'
 // style
 import './style.css'
 
+
 const main = app(state, actions, Root, document.getElementById("app"))
 
-// watch
+// watch clipboard
 setInterval(main.clipboardChange, 1000, 1) 
 ipcRenderer.on("translate-result", (event : any, args : any) => {
   main.receive(args);
 })
+
+// alwaysOnTop
+remote.getCurrentWindow().setAlwaysOnTop(state.config.alwaysOnTop);
